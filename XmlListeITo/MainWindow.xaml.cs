711c4fbd8da4c6476b09b2219b74b9c047c80592
Dragan Pepic <d.pepic@hotmail.com>
2020-07-28 01:30:11 +0200

    Add project files.

---
+++ XmlListeITo/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XmlListeITo
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			List<string> nekaLista = new List<string>();
+			nekaLista.Add("jen");
+			nekaLista.Add("dva");
+			nekaLista.Add("tri");
+			nekaLista.Add("cetir");
+			Dictionary<string, int> recnik = new Dictionary<string, int>();
+			recnik.Add("test", 2);
+			XmlSerializer xmlKonvert = new XmlSerializer(nekaLista.GetType());
+			StringWriter upisivac = new StringWriter();
+			MemoryStream ms = new MemoryStream();
+			xmlKonvert.Serialize(upisivac, nekaLista);
+			string bla = upisivac.ToString();
+
+			List<string> bla2;
+			using (TextReader tr = new StringReader(bla)) 
+				bla2 = xmlKonvert.Deserialize(tr) as List<string>;
+
+			MessageBox.Show(bla2.Count.ToString());
+		}
+	}
+
+	[TestClass]
+	public class Testing
+	{
+		[TestMethod]
+		public void TestM()
+		{
+			int x = 5;
+			int y = 6;
+
+			Assert.AreEqual(x, y);
+		}
+	}
+}
